1d6b5a73d2854eef4e7f2d84acbefdbf63df015b
328346602 <328346602@qq.com>
2015-06-09 23:41:37 +0800

    2015-06-09晚
    重写Log类,重写CK类(未完成)

--- CM.Map/Log.cs
+++ CM.Map/Log.cs
@@ -10,10 +10,46 @@ namespace CM.Map
         /// <summary>
         /// 记录日志
         /// </summary>
-        /// <param name="msg"></param>
-        public static void WriteLog(string text)
+        /// <param name="sMsg"></param>
+        public static void WriteLog(string sMsg)
         {
+            Write("Log", sMsg);
+        }
 
+        /// <summary>
+        /// 记录错误信息
+        /// </summary>
+        /// <param name="sMsg"></param>
+        public static void WriteError(string sMsg)
+        {
+            Write("Error", sMsg);
+        }
+
+        /// <summary>
+        /// 记录调试信息
+        /// </summary>
+        /// <param name="sMsg"></param>
+        public static void WriteDebug(string sMsg)
+        {
+            Write("Debug", sMsg);
+        }
+
+        /// <summary>
+        /// 写异常信息
+        /// </summary>
+        /// <param name="ex"></param>
+        public static void WriteException(Exception ex)
+        {
+            Write("Exception", ex.Message);
+        }
+
+        /// <summary>
+        /// 写日志基础方法
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="sMsg"></param>
+        public static void Write(string type,string sMsg)
+        {
             string path = AppDomain.CurrentDomain.BaseDirectory;
             path = System.IO.Path.Combine(path, "Logs");
 
@@ -22,11 +58,11 @@ namespace CM.Map
                 System.IO.Directory.CreateDirectory(path);
             }
             //string fileFullName = System.IO.Path.Combine(path, string.Format("{0}.log", DateTime.Now.ToString("yyMMdd-HHmmss")));
-            string fileFullName = System.IO.Path.Combine(path, string.Format("{0}.log", DateTime.Now.ToString("yyMMdd")));
+            string fileFullName = System.IO.Path.Combine(path, string.Format("{0}{1}.log", DateTime.Now.ToString("yyMMdd"), type));
 
             using (StreamWriter output = System.IO.File.AppendText(fileFullName))
             {
-                output.WriteLine(DateTime.Now.ToString()+">>>>"+ text);
+                output.WriteLine(DateTime.Now.ToString() + ">>>>" + sMsg);
 
                 output.Close();
             }
